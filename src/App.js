71e5585768e1ae15ccb3185276ce1841ed1e18c5
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2020-12-12 12:38:44 -0500

    navbar and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
+import DmNav from './components/DmNav'
 
 const App = () => {
     return (
         <div>
             <Header/>
+            <DmNav/>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,20 @@ import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const Header= () => {
+
+    const jubmoStyle = {
+        marginBottom: 0,
+        color: 'white',
+        backgroundImage: 'url(https://cdn.pixabay.com/photo/2019/08/22/08/30/fantasy-4422921_960_720.jpg)',
+
+    }
+
     return (
-        <Jumbotron fluid>
+        <Jumbotron fluid style={jubmoStyle} >
         <Container>
-            <h1>Fluid jumbotron</h1>
+            <h1>DM Reference</h1>
             <p>
-            This is a modified jumbotron that occupies the entire horizontal space of
-            its parent.
+                Common Topics and Areas for Every Dungeon Master
             </p>
         </Container>
         </Jumbotron>
