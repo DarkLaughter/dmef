fb2ae3b79d3fb3598162321de8e17c42420fde01
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2020-12-17 22:44:20 -0500

    standard page components

---
+++ src/components/PageDefault.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageDefault = () => {
+    return (
+        <div>
+            <p>no search results</p>
+        </div>
+    )
+}
+
+export default PageDefault
---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchPage = () => {
+    return (
+        <div>
+           <p>Search results</p> 
+        </div>
+    )
+}
+
+export default SearchPage
